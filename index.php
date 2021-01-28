b3b1c67b98d8e41da486049bdd05fc0c596e465f
Tina Ostrander <tostrander@greenriver.edu>
2021-01-28 14:28:39 -0500

    Added controller and htaccess

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/** Create a food order form */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    echo "Home page";
+    //Display a view
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+//Define an order route
+$f3->route('GET /order', function() {
+
+    echo "Order route";
+});
+
+//Define an order2 route
+$f3->route('GET /order2', function() {
+
+    echo "Order 2 route";
+});
+
+//Define a summary route
+$f3->route('GET /summary', function() {
+
+    echo "Summary route";
+});
+
+//Run Fat-Free
+$f3->run();
